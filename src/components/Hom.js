f76a09f717c07f99f3999007c2b3379292a94e48
Suryansh <shahisuryansh28@gmail.com>
2021-10-22 13:48:07 +0530

    first commit

---
+++ src/components/Hom.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from 'styled-components'
+import Section from './Section'
+function Hom() {
+    return (
+        <Wrap>
+            <Section title = "Model S" description ="Order Online for Touchless Delivery"
+              backgroundImg = "model-s.jpg" 
+               leftBtnText = "Custom order"
+                rightBtnText = "Existing Inventory"
+            />
+             <Section title = "Model Y" description ="Order Online for Touchless Delivery"
+              backgroundImg = "model-y.jpg" 
+               leftBtnText = "Custom order"
+                rightBtnText = "Existing Inventory"
+            />
+             <Section title = "Model 3" description ="Order Online for Touchless Delivery"
+              backgroundImg = "model-3.jpg" 
+               leftBtnText = "Custom order"
+                rightBtnText = "Existing Inventory"
+            />
+             <Section title = "Model X" description ="Order Online for Touchless Delivery"
+              backgroundImg = "model-x.jpg" 
+               leftBtnText = "Custom order"
+                rightBtnText = "Existing Inventory"
+            />
+            <Section title = "Lowest Cost Solar Panels in India"
+            description = "Money-back guarantee"
+            backgroundImg = "solar-panel.jpg"
+            leftBtnText = "Order Now"
+            rightBtnText = "Learn More"
+            
+            />
+            <Section
+            title = "Accessories"
+            description =""
+            backgroundImg = "accessories.jpg"
+            leftBtnText = "Shop Now"
+            />
+        </Wrap>
+    )
+}
+
+export default Hom
+
+const Wrap = styled.div`
+
+height:100vh ;
+
+`
